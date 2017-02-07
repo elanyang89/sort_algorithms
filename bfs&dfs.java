a3c4410803ab490a82c63aabe3f5247f7910890c
elanyang89 <380822851@qq.com>
2017-02-07 16:34:09 +0800

    2.fix

--- bfs&dfs.java
+++ bfs&dfs.java
@@ -31,12 +31,21 @@ public class Nodeinfo{
     }
 
     void onCreateGraph(){
+<<<<<<< Updated upstream
         Node a = new Node('A');
         Node b = new Node('B');
         Node c = new Node('C');
         Node d = new Node('D');
         Node e = new Node('E');
         Node f = new Node('F');
+=======
+        Node a = new Node('a');
+        Node b = new Node('b');
+        Node c = new Node('c');
+        Node d = new Node('d');
+        Node e = new Node('e');
+        Node f = new Node('f');
+>>>>>>> Stashed changes
 
         List<Node> aList = Arrays.asList(a, b, c, d);
         List<Node> bList = Arrays.asList(b, a, e, f);
